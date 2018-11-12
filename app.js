3fd411dca8291edb57ad24814f472a97835cacde
Soheima Canton <soheima@Soheimas-MacBook-Pro.local>
2018-11-12 22:36:37 +0100

    added port

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+
 const app = express()
 
 app.use(express.static('public'))
@@ -7,4 +8,4 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname + '/index.html');
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
